bdb502a7827e5c0c81dfa9c886968ea6d5bc5ee2
Vinícius Regis <vinicius.regis@griinstitute.org>
2026-02-02 09:22:15 -0300

    prevent showing code in markdown

--- src/components/thread/markdown-text.tsx
+++ src/components/thread/markdown-text.tsx
@@ -207,36 +207,37 @@ const defaultComponents: any = {
     className?: string;
     children: React.ReactNode;
   }) => {
-    const match = /language-(\w+)/.exec(className || "");
+    return <></>;
+    // const match = /language-(\w+)/.exec(className || "");
 
-    if (match) {
-      const language = match[1];
-      const code = String(children).replace(/\n$/, "");
+    // if (match) {
+    //   const language = match[1];
+    //   const code = String(children).replace(/\n$/, "");
 
-      return (
-        <>
-          <CodeHeader
-            language={language}
-            code={code}
-          />
-          <SyntaxHighlighter
-            language={language}
-            className={className}
-          >
-            {code}
-          </SyntaxHighlighter>
-        </>
-      );
-    }
+    //   return (
+    //     <>
+    //       <CodeHeader
+    //         language={language}
+    //         code={code}
+    //       />
+    //       <SyntaxHighlighter
+    //         language={language}
+    //         className={className}
+    //       >
+    //         {code}
+    //       </SyntaxHighlighter>
+    //     </>
+    //   );
+    // }
 
-    return (
-      <code
-        className={cn("rounded font-semibold", className)}
-        {...props}
-      >
-        {children}
-      </code>
-    );
+    // return (
+    //   <code
+    //     className={cn("rounded font-semibold", className)}
+    //     {...props}
+    //   >
+    //     {children}
+    //   </code>
+    // );
   },
 };
 
